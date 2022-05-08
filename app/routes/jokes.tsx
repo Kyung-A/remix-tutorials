b9034b1af88173dc90e68153ecf09730e97ed4e5
Nam Eungyeong <nek1717@naver.com>
2022-05-08 17:24:22 +0900

    feat: joke data rendering

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,13 +1,36 @@
-import type { LinksFunction } from "@remix-run/node";
-import { Outlet, Link } from "@remix-run/react";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import { Outlet, Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
 
 import stylesUrl from '~/styles/jokes.css';
+import { db } from "~/utils/db.server";
 
 export const links: LinksFunction = () => {
     return [{ rel: 'stylesheet', href: stylesUrl }];
 };
 
+type LoaderData = {
+    jokeListItems: Array<{ id: string; name: string }>;
+};
+
+// 데이터를 가져온다 (서버에서 실행되는 코드임)
+export const loader: LoaderFunction = async () => {
+    const data: LoaderData = {
+        jokeListItems: await db.joke.findMany(), // 조건에 맞는 객체들을 배열에 담아서 반환
+
+        // 특정 항목 호출
+        // jokeListItems: await db.joke.findMany({
+        //     take: 3, // index 0~2만 받아옴
+        //     select: { id: true, name: true }, // id, name key 받아와서 렌더링
+        //     orderBy: { createAt: 'desc' }
+        // })
+    };
+    return json(data);
+}
+
 export default function JokesRoute() {
+    const data = useLoaderData<LoaderData>();
+
     return (
         <div className="jokes-layout">
             <header className="jokes-header">
@@ -29,9 +52,13 @@ export default function JokesRoute() {
                         <p>Here are a few more jokes to check out:</p>
 
                         <ul>
-                            <li>
-                                <Link to="some-joke-id">Hippo</Link>
-                            </li>
+                            {data.jokeListItems.map((joke) => (
+                                <li key={joke.id}>
+                                    <Link to={joke.id}>
+                                        {joke.name}
+                                    </Link>
+                                </li>
+                            ))}
                         </ul>
                         <Link to="new" className="button">
                             Add your own
--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,38 @@
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { Link, useLoaderData } from "@remix-run/react";
+import type { Joke } from '@prisma/client';
+
+import { db } from '~/utils/db.server';
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async ({
+    params
+}) => {
+    const joke = await db.joke.findUnique({ // where에 적힌 조건으로 db에서 해당 조건에 맞는 하나의 데이터를 반환
+        where: { id: params.jokeId },
+    });
+    if (!joke) {
+        throw new Error('찾을 수 없습니다.');
+    }
+
+    const data: LoaderData = { joke };
+    return json(data);
+}
+
 export default function JokeRoute() {
+    const data = useLoaderData<LoaderData>();
+
     return (
         <div>
             <p>Here's your hilarious joke:</p>
             <p>
-                Why don't you find hippoptamuses hiding in trees?
-                They're really good at it.
+                {data.joke.content}
             </p>
+            <Link to=".">
+                {data.joke.name} Permalink
+            </Link>
         </div>
     );
 }
